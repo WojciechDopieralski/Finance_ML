a82778fac754f3dd09c10335201cebbb10105790
Sajjadina <pafi.main@gmail.com>
2022-03-13 20:51:41 +0100

    dodanie funkcji pobierającej i drukującej wyniki z CSV

--- Finance_ML/Finance_ML.py
+++ Finance_ML/Finance_ML.py
@@ -1,1 +1,15 @@
+# CONVICTIONSUMMARY_270_opt3_best2(1).csv
 
+import csv
+
+with open('Assets\CONVICTIONSUMMARY_270_opt3_best2(1).csv') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter='|')
+    line_count = 0
+    for row in csv_reader:
+        if line_count == 0:
+            print(f'Column names are {", ".join(row)}')
+            line_count += 1
+        else:
+            print(f'\t{row[0]} asdadsa {row[1]} ')
+            line_count += 1
+    print(f'Processed {line_count} lines.')
